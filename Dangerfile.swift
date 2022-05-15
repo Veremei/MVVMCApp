fb449916daccfe7a8875ce0973ce7c6e3602973a
Daniel Veramei <daniil_veramei@epam.com>
2022-05-15 17:20:07 +0300

    add xcov

--- Dangerfile.swift
+++ Dangerfile.swift
@@ -4,3 +4,10 @@ import Foundation
 let danger = Danger()
 let editedFiles = danger.git.modifiedFiles + danger.git.createdFiles
 message("These files have changed: \(editedFiles.joined(separator: ", "))")
+
+xcov.report(
+    scheme: 'MVVMCApp',
+    workspace: 'MVVMCApp/project.xcworkspace',
+    minimum_coverage_percentage: 90
+)
+
